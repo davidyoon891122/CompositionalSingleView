ad0bcd01f4d6cb13ef3971d65ffc09301243e851
Davidyoon <davidyoon@koreainvestment.com>
2023-07-14 17:20:45 +0900

    Shuffle the mock data
    * Shuffle the mock data

--- CompositionalSingleViewApp/Model/Setting/ProductModel.swift
+++ CompositionalSingleViewApp/Model/Setting/ProductModel.swift
@@ -59,66 +59,66 @@ extension ProductModel {
         switch menu {
         case .updown:
             return [
-                ProductModel(imageName: "wework", name: "등락 역삼 위워크", price: 4850, priceValue: 250, priceValuePercent: 5.75),
-                ProductModel(imageName: "kyobo", name: "등락 여의도 교보증권 건물", price: 3250, priceValue: 930, priceValuePercent: -10.5),
                 ProductModel(imageName: "mirae", name: "등락 종각 미래에셋 건물", price: 4900, priceValue: 550, priceValuePercent: 19.75),
                 ProductModel(imageName: "lotte", name: "등락 명동 롯데 호첼", price: 2550, priceValue: -1500, priceValuePercent: -42.50),
+                ProductModel(imageName: "wework", name: "등락 역삼 위워크", price: 4850, priceValue: 250, priceValuePercent: 5.75),
+                ProductModel(imageName: "kyobo", name: "등락 여의도 교보증권 건물", price: 3250, priceValue: 930, priceValuePercent: -10.5),
                 ProductModel(imageName: "trade", name: "등락 삼성 무역센터", price: 5550, priceValue: 2050, priceValuePercent: 32.25),
                 ProductModel(imageName: "ifc", name: "등락 여의도 ifc", price: 4250, priceValue: 750, priceValuePercent: 15.75),
             ]
         case .up:
             return [
+                ProductModel(imageName: "trade", name: "상승 삼성 무역센터", price: 5550, priceValue: 2050, priceValuePercent: 32.25),
+                ProductModel(imageName: "ifc", name: "상승 여의도 ifc", price: 4250, priceValue: 750, priceValuePercent: 15.75),
                 ProductModel(imageName: "wework", name: "상승 역삼 위워크", price: 4850, priceValue: 250, priceValuePercent: 5.75),
                 ProductModel(imageName: "kyobo", name: "상승 여의도 교보증권 건물", price: 3250, priceValue: 930, priceValuePercent: -10.5),
                 ProductModel(imageName: "mirae", name: "상승 종각 미래에셋 건물", price: 4900, priceValue: 550, priceValuePercent: 19.75),
                 ProductModel(imageName: "lotte", name: "상승 명동 롯데 호첼", price: 2550, priceValue: -1500, priceValuePercent: -42.50),
-                ProductModel(imageName: "trade", name: "상승 삼성 무역센터", price: 5550, priceValue: 2050, priceValuePercent: 32.25),
-                ProductModel(imageName: "ifc", name: "상승 여의도 ifc", price: 4250, priceValue: 750, priceValuePercent: 15.75),
             ]
         case .dividend:
             return [
+                ProductModel(imageName: "mirae", name: "배당 종각 미래에셋 건물", price: 4900, priceValue: 550, priceValuePercent: 19.75),
                 ProductModel(imageName: "wework", name: "배당 역삼 위워크", price: 4850, priceValue: 250, priceValuePercent: 5.75),
                 ProductModel(imageName: "kyobo", name: "배당 여의도 교보증권 건물", price: 3250, priceValue: 930, priceValuePercent: -10.5),
-                ProductModel(imageName: "mirae", name: "배당 종각 미래에셋 건물", price: 4900, priceValue: 550, priceValuePercent: 19.75),
-                ProductModel(imageName: "lotte", name: "배당 명동 롯데 호첼", price: 2550, priceValue: -1500, priceValuePercent: -42.50),
                 ProductModel(imageName: "trade", name: "배당 삼성 무역센터", price: 5550, priceValue: 2050, priceValuePercent: 32.25),
                 ProductModel(imageName: "ifc", name: "배당 여의도 ifc", price: 4250, priceValue: 750, priceValuePercent: 15.75),
+                ProductModel(imageName: "lotte", name: "배당 명동 롯데 호첼", price: 2550, priceValue: -1500, priceValuePercent: -42.50),
             ]
         case .dividenddate:
             return [
-                ProductModel(imageName: "wework", name: "배당일 역삼 위워크", price: 4850, priceValue: 250, priceValuePercent: 5.75),
+                ProductModel(imageName: "ifc", name: "배당일 여의도 ifc", price: 4250, priceValue: 750, priceValuePercent: 15.75),
                 ProductModel(imageName: "kyobo", name: "배당일 여의도 교보증권 건물", price: 3250, priceValue: 930, priceValuePercent: -10.5),
                 ProductModel(imageName: "mirae", name: "배당일 종각 미래에셋 건물", price: 4900, priceValue: 550, priceValuePercent: 19.75),
                 ProductModel(imageName: "lotte", name: "배당일 명동 롯데 호첼", price: 2550, priceValue: -1500, priceValuePercent: -42.50),
                 ProductModel(imageName: "trade", name: "배당일 삼성 무역센터", price: 5550, priceValue: 2050, priceValuePercent: 32.25),
-                ProductModel(imageName: "ifc", name: "배당일 여의도 ifc", price: 4250, priceValue: 750, priceValuePercent: 15.75),
+                ProductModel(imageName: "wework", name: "배당일 역삼 위워크", price: 4850, priceValue: 250, priceValuePercent: 5.75),
             ]
         case .volume:
             return [
-                ProductModel(imageName: "wework", name: "거래량 역삼 위워크", price: 4850, priceValue: 250, priceValuePercent: 5.75),
-                ProductModel(imageName: "kyobo", name: "거래량 여의도 교보증권 건물", price: 3250, priceValue: 930, priceValuePercent: -10.5),
-                ProductModel(imageName: "mirae", name: "거래량 종각 미래에셋 건물", price: 4900, priceValue: 550, priceValuePercent: 19.75),
                 ProductModel(imageName: "lotte", name: "거래량 명동 롯데 호첼", price: 2550, priceValue: -1500, priceValuePercent: -42.50),
                 ProductModel(imageName: "trade", name: "거래량 삼성 무역센터", price: 5550, priceValue: 2050, priceValuePercent: 32.25),
                 ProductModel(imageName: "ifc", name: "거래량 여의도 ifc", price: 4250, priceValue: 750, priceValuePercent: 15.75),
+                ProductModel(imageName: "wework", name: "거래량 역삼 위워크", price: 4850, priceValue: 250, priceValuePercent: 5.75),
+                ProductModel(imageName: "kyobo", name: "거래량 여의도 교보증권 건물", price: 3250, priceValue: 930, priceValuePercent: -10.5),
+                ProductModel(imageName: "mirae", name: "거래량 종각 미래에셋 건물", price: 4900, priceValue: 550, priceValuePercent: 19.75),
             ]
         case .marketcap:
             return [
                 ProductModel(imageName: "wework", name: "시가총액 역삼 위워크", price: 4850, priceValue: 250, priceValuePercent: 5.75),
-                ProductModel(imageName: "kyobo", name: "시가총액 여의도 교보증권 건물", price: 3250, priceValue: 930, priceValuePercent: -10.5),
                 ProductModel(imageName: "mirae", name: "시가총액 종각 미래에셋 건물", price: 4900, priceValue: 550, priceValuePercent: 19.75),
                 ProductModel(imageName: "lotte", name: "시가총액 명동 롯데 호첼", price: 2550, priceValue: -1500, priceValuePercent: -42.50),
                 ProductModel(imageName: "trade", name: "시가총액 삼성 무역센터", price: 5550, priceValue: 2050, priceValuePercent: 32.25),
                 ProductModel(imageName: "ifc", name: "시가총액 여의도 ifc", price: 4250, priceValue: 750, priceValuePercent: 15.75),
+                ProductModel(imageName: "kyobo", name: "시가총액 여의도 교보증권 건물", price: 3250, priceValue: 930, priceValuePercent: -10.5),
             ]
         case .landprice:
             return [
+                ProductModel(imageName: "ifc", name: "공시지가 여의도 ifc", price: 4250, priceValue: 750, priceValuePercent: 15.75),
                 ProductModel(imageName: "wework", name: "공시지가 역삼 위워크", price: 4850, priceValue: 250, priceValuePercent: 5.75),
-                ProductModel(imageName: "kyobo", name: "공시지가 여의도 교보증권 건물", price: 3250, priceValue: 930, priceValuePercent: -10.5),
                 ProductModel(imageName: "mirae", name: "공시지가 종각 미래에셋 건물", price: 4900, priceValue: 550, priceValuePercent: 19.75),
                 ProductModel(imageName: "lotte", name: "공시지가 명동 롯데 호첼", price: 2550, priceValue: -1500, priceValuePercent: -42.50),
                 ProductModel(imageName: "trade", name: "공시지가 삼성 무역센터", price: 5550, priceValue: 2050, priceValuePercent: 32.25),
-                ProductModel(imageName: "ifc", name: "공시지가 여의도 ifc", price: 4250, priceValue: 750, priceValuePercent: 15.75),
+                ProductModel(imageName: "kyobo", name: "공시지가 여의도 교보증권 건물", price: 3250, priceValue: 930, priceValuePercent: -10.5),
             ]
         }
     }
